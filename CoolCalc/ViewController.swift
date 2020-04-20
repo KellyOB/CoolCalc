0a83a5786b160206834f12e6f314c92d5c549503
Kelly O'Brien <kelly@kismetdevelopment.com>
2020-04-19 17:30:21 -0700

    Initial Commit

---
+++ CoolCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoolCalc
+//
+//  Created by Kelly O'Brien on 4/19/20.
+//  Copyright © 2020 Kismet Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
